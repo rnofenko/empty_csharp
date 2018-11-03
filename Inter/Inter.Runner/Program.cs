413ba36b1c71e9c3e48dd22050d409e461278a54
Roman Nofenko <rnofenko@gmail.com>
2018-11-03 12:17:05 -0400

    empty project

---
+++ Inter/Inter.Runner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Inter.Runner
+{
+    class Program
+    {
+        static void Main()
+        {
+            var res = new Calculator().Calc(1, 2);
+            Console.WriteLine($"Result = {res}");
+            Console.ReadLine();
+        }
+    }
+
+    public class Calculator
+    {
+        public int Calc(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Inter/Inter.Tests/CalculatorTest.cs
@@ -0,0 +1,16 @@
+﻿using Inter.Runner;
+using NUnit.Framework;
+
+namespace Inter.Tests
+{
+    public class CalculatorTest
+    {
+        [Test]
+        public void A_and_B()
+        {
+            var calculator = new Calculator();
+            var res = calculator.Calc(1, 2);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
